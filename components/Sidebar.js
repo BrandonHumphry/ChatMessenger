4d1ed78cb98ccd03b86fd34378b5f379e063814f
Brandon Humphry <brandonhumphry@gmail.com>
2021-04-07 23:52:59 -0700

    building out login page

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,14 +3,20 @@ import styled from "styled-components";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
+import * as EmailValidator from "email-validator";
 
 function Sidebar() {
   const createChat = () => {
     const input = prompt(
-      "Please enter an email address for the user you wish to chat with");
+      "Please enter an email address for the user you wish to chat with"
+    );
   };
-   
-}
+  if (!input) return null;
+
+  if (EmailValidator.validate(input)) {
+    //  add chat into db 'chats' collection
+  }
+
   return (
     <Container>
       <Header>
