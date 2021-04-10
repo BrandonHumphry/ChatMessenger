1d7a59f44f53d978ebecb2fb048f0f82112dacf3
Brandon Humphry <brandonhumphry@gmail.com>
2021-04-09 20:46:12 -0700

    userAuth working, clicking avatar will sign user out and redirect to Google login

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,7 @@
 import { Circle } from "better-react-spinkit";
 function Loading() {
   return (
-    <center>
+    <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
         <img
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
