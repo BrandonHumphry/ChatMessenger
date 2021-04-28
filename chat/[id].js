210d10a61ffaa476e46cd8ff037c0560e2981d3c
Brandon Humphry <brandonhumphry@gmail.com>
2021-04-27 22:40:31 -0700

    building out some of the chat page's structure for items to be server side rendered into

--- chat/[id].js
+++ chat/[id].js
@@ -1,5 +1,39 @@
+import styled from "styled-components";
+import Sidebar from "../../components/Sidebar";
+import ChatScreen from "../components/ChatScreen";
 function Chat() {
-  return <div></div>;
+  return (
+    <Container>
+      <Head>
+        <title>Chat</title>
+        <h1>This is a chat</h1>
+      </Head>
+      <Sidebar />
+      <ChatContainer>
+        <ChatScreen />
+      </ChatContainer>
+    </Container>
+  );
 }
 
 export default Chat;
+
+export async function getServerSideProps(context) {
+  const ref = db.collection("chats").doc(context.query.id);
+}
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ChatContainer = styled.div`
+  flex: 1;
+  overflow: scroll;
+  height: 100vh;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  --ms-overflow-style: none;
+  scrollbar-width: none;
+`;
