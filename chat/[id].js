0b01654897b4cdf8ac3c3d092bf7ac00d2b509da
Brandon Humphry <brandonhumphry@gmail.com>
2021-04-28 23:19:13 -0700

    functions and mapping for server side rendering

--- chat/[id].js
+++ chat/[id].js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import Sidebar from "../../components/Sidebar";
 import ChatScreen from "../components/ChatScreen";
-function Chat() {
+import { db } from "firebase";
+
+function Chat({ chat, messages }) {
   return (
     <Container>
       <Head>
@@ -20,6 +22,33 @@ export default Chat;
 
 export async function getServerSideProps(context) {
   const ref = db.collection("chats").doc(context.query.id);
+
+  // PREP messages on the server
+  const messageRes = await ref
+    .collection("messages")
+    .orderBy("timestamp", "asc")
+    .get();
+
+  const messages = messageRes.docs
+    .map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }))
+    .map(messages => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime()
+    }));
+
+  // PREP the chats
+  const chatRes = await ref.get();
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data()
+  };
+
+  return {
+    props: { messages: JSON.stringify(messages), chat: chat }
+  };
 }
 
 const Container = styled.div`
